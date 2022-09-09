bc2bd1d29c09ead64b011fb61697d4c15a932802
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-08 17:57:36 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* This program is to print "Programming is like building a multilingual puzzle"
+ *
+ *
+ * main - "Programming is like building a multilingual puzzle" followed by new line.
+ *
+ *
+ * Return always 0 (success)
+ *
+ */
+
+ 	int main (void)
+
+{
+
+
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+
+
+
+	return (0);
+
+
+
+}
