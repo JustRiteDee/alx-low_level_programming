1fff27bff9501604c5ff9d35f733c65c44210a3d
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-08 19:58:00 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  *  main - Prints proper grammar, but the outcome is a piece of art.
  *
- *  Retun: Always zero 
+ *  Retun: Always to zero (0)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
