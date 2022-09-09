d539a6b3586797b07df18c839087567f75b2dd2a
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-08 18:40:34 -0700

    with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  main - Prints with proper grammar, but the outcome is a piece of art.
+ *
+ *  Retun: Always (0) 
+ *
+ */
+int main(void)
+
+{	/*declaration*/
+	char ephy = "with proper grammar, but the outcome is a piece of art,"
+
+	/*statement*/
+	printf(ephy);
+
+	return (0)
+
+}
