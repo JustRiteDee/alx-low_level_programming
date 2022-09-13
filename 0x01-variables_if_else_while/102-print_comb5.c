5fcebad2c07ab18320b79614ae7736807d31567d
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-13 06:47:21 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,23 +12,25 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 89; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-			if (!(i == 89 && j == 99))
-				putchar(',');
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
-
-
 	}
-
 	putchar('\n');
 	return (0);
 }
