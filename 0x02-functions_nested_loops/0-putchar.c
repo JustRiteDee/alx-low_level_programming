fe2128895fa9fdf15ac75fc1cebf0d5665001892
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-13 16:34:34 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,16 @@
  *
  * Author: Emmanuel Ephraim
  */
-#include "main.h"
 #include <unistd.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - writes the character ch to stdout
+ * @ch: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned and errno is set appropriately.
  */
-int _putchar(char c)
+int _putchar(char ch)
 {
-	return (write(1, &c, 1));
+	return (write(1, &ch, 1));
 
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,14 @@
 /**
  * print_alphabet - Utilizes the _putchar function to print
  * the alphabets a - z
+ *
  * Return: zero (success)
  */
 void print_alphabet(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ++ch)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
 }
