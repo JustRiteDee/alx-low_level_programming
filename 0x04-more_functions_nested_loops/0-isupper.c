b4566a9dac3186a894dfe415f4d542933589a710
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-15 14:39:40 -0700

    I don come

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase, 0 if not.
+ */
+
+int _isupper(int c)
+{
+	int c;
+	
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	else
+
+	{
+
+		return (0);
+
+	}
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+More functions amd nested loops.
