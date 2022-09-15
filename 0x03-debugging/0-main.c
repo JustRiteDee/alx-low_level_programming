63905aae23bc0f66147c992d8d2a2926a65e252f
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-14 19:24:23 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Tests function that prints if positive or negative
+ * Return: Always zero (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0; /* Initializing the variable to zero*/
+	positive_or_negetive(i); /*calling up the function: positive_or_negative*/
+
+	return (0);
+}
