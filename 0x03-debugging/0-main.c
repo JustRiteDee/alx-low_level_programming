702262b345098a91b3c5cdf79212ce3a91b202a1
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-14 19:46:40 -0700

    my degugging tasks done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,8 +7,8 @@ int main(void)
 {
 	int i;
 
-	i = 0; /* Initializing the variable to zero*/
-	positive_or_negetive(i); /*calling up the function: positive_or_negative*/
+	i = 0; // Initializing the variable to zero
+	positive_or_negetive(i); // calling up the function: positive_or_negative
 
 	return (0);
 }
