72395bac5e726782281cd848fa31ae468300b777
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-19 22:25:11 -0700

    we're here again

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * reset_t0_98 - takes a pointer to a parameter and updates its value.
+ * reset_to_98 - takes a pointer to a parameter and updates its value.
  * @n: input integer.
- *
  * Return: no return.
  */
 void reset_to_98(int *n)
