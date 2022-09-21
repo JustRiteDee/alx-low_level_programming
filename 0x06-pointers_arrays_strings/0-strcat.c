872c6bfff5a00a88776869471563d16074e30394
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-21 15:21:47 -0700

    strcat function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - This is a function strcat
+ * @dest: first parameter
+ * src: second parameter
+ *
+ * Return: Always returns a string
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	dest[len] = '\0';
+	return (dest);
+}
