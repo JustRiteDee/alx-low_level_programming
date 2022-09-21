be4c6e7d9a88b9d4f9a38e6bb58e6f9010f0ed80
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-21 15:50:43 -0700

    strcat done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,6 +11,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat (dest, src);
+	int len = 0, 1;
+
+	while (dest[len])
+		len++;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
 	return (dest);
 }
