0678b54c308fae961a8cbfb480fab08e45e1b2f5
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-09-22 17:44:51 -0700

    crime is inevitable

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main(void)
-
 {
   int n;
   int a[5];
@@ -21,8 +20,4 @@ int main(void)
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
-};
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
 }
