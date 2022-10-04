c6e66f6f6ba01acecea9e2b23bf5c49f50ebb6a8
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-10-04 04:50:39 +0100

    okponku

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,25 +3,6 @@
 #include "main.h"
 
 /**
- * ch_free_grid - frees a 2 dimensional array.
- * @grid: multidimensional array of char.
- * @height: height of the array.
- *
- * Return: no return
- */
-void ch_free_grid(char **grid, unsigned int height)
-{
-	if (grid != NULL && height != 0)
-	{
-		for (; height > 0; height--)
-			free(grid[height]);
-		free(grid[height]);
-		free(grid);
-	}
-}
-
-
-/**
 * count_words - function that count words in a string
 * separated by a blank space
 * Return: number of words in the string
