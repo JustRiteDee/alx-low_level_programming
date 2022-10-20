4c22e7af69172745bc60fa05b8a49bb0f9d9ddc0
Emmanuel Ephraim <emmanueldominic.ephraim@gmail.com>
2022-10-20 23:22:26 +0100

    hare print

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,11 +8,9 @@
  
 /**
  * _100first - Prints a message before main function
- * Return - returns 0
  */
  
 void __attribute__ ((constructor)) _100first(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-	return(0);
 }
